58721231399581cdb3bc35f296985d551d02ede3
Rafael Perez-Torro <rafael.perez@p3-group.com>
2022-02-01 23:35:11 +0000

    [ADD] Lecture 10: EC2 instance

--- src/main.tf
+++ src/main.tf
@@ -1,3 +1,8 @@
 provider "aws" {
-  region = "eu-west-1"
+  region = "eu-west-2"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0015a39e4b7c0966f"
+  instance_type = "t2.micro"
 }
